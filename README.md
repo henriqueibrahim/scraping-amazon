e75017ad30621c9af0f42a17948bc5a97a32daa7
henriqueibrahim <henriqueibrahim7@gmail.com>
2022-06-23 12:26:06 -0300

    Adicionando código e alterando o README

--- README.md
+++ README.md
@@ -1,2 +1,74 @@
 # scraping-amazon
-Scrap resultado de pesquisa na amazon
+
+* Meu primeiro projeto. Um scraper que raspa os resultados de uma pesquisa na amazon e salva em um banco sql
+* Ainda em desenvolvimento
+* Não está optimizado nem com a melhor lógica
+* To aprendendo ainda :)
+
+
+# Executando o script
+
+Para executar o script é necessário alguns passos antes:
+
+1. Instalar os requirements
+2. Configuração do banco de dados
+3. Inserir as informações no arquivo .env
+
+
+Para instalar os requirements sugiro fazer dentro de um ambiente virtual do python para não interferir com seu sistema
+
+```
+python3 -m venv virtualenvironment
+```
+Depois só dar o source
+
+```
+source virtualenvironment/bin/activate
+```
+
+Agora só instalar
+
+```
+pip install -r requirements.txt
+```
+
+
+**Vocẽ já deve ter o banco de dados instalado e rodando na máquina**
+
+
+Crie a database:
+
+```
+CREATE DATABASE nomequequiser;
+```
+
+Depois de selecionada (```USE nomequequiser; ```), é preciso criar as tables
+
+```
+CREATE TABLE Data (
+   title VARCHAR(500) NOT NULL,
+   price VARCHAR(10) NOT NULL,
+   PRIMARY KEY (title, price)
+);
+```
+
+*É meu primeiro contato com SQL, não sei se é a melhor maneira de se fazer isso*
+*Coloco o PRIMARY KEY para ele evitar duplicados e não salvar 2x a mesma coisa*
+
+
+Depois de criada, se deve criar um arquivo .env com as informações da database
+
+```
+USER=usuárioDoBancoSQL
+PASSWORD=senhaDoBancoSQL
+DATABASE=nomeDaDatabase
+```
+
+Depois disso, só entrar no arquivo "search_urls.txt" e colocar as URLs de pesquisa que deseja raspar (lembrando que somente da amazon) e rodar o main.py
+segue o padrão que já vai no arquivo
+
+
+# TODO:
+
+1. Precisa passar para as outras páginas de pesquisa, só está raspando a primeira
+2. Tem mais coisa pra fazer mas por enquanto só lembro disso rs
---
+++ main.py
@@ -0,0 +1,77 @@
+import json
+import mysql.connector
+import os
+import requests
+from dotenv import load_dotenv
+from selectorlib import Extractor
+from time import sleep
+
+# Loading and setting SQL variables.
+# Carregando as variáveis do SQL.
+load_dotenv()
+
+sqlUser = os.getenv("USER")
+sqlPassword = os.getenv("PASSWORD")
+sqlDatabase = os.getenv("DATABASE")
+
+
+# Create an Extractor by reading from the YAML file.
+# Cria um Extractor lendo do arquivo YAML.
+e = Extractor.from_yaml_file('search.yml')
+
+
+def scrape(url):  
+
+    headers = {
+        'dnt': '1',
+        'upgrade-insecure-requests': '1',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246',
+        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        'sec-fetch-site': 'same-origin',
+        'sec-fetch-mode': 'navigate',
+        'sec-fetch-user': '?1',
+        'sec-fetch-dest': 'document',
+        'referer': 'https://www.amazon.com/',
+        'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
+    }
+
+    print(f"Downloading {url}")
+    r = requests.get(url, headers=headers)
+
+    # A simple check to see if the page was blocked.
+    # Verificação simples para ver se a página foi bloqueada.
+    if r.status_code > 500:
+        if "To discuss automated access to Amazon data please contact" in r.text:
+            print(f"Page {url} was blocked by Amazon.\n")
+        else:
+            print(f"Page {url} must have been blocked by Amazon.The status code is: {r.status_code}")
+        return None
+    # Pass the HTML of the page and create 
+    return e.extract(r.text)
+
+
+# Connect no MySQL.
+# Conectar no MySQL.
+cnx = mysql.connector.connect(user=sqlUser, password=sqlPassword, database=sqlDatabase)
+cursor = cnx.cursor()
+
+with open("search_urls.txt", "r") as urllist:
+    for url in urllist.read().splitlines():
+        data = scrape(url)
+        if data:
+            for product in data["products"]:
+                product["search_url"] = url
+
+                sqlTitle = product.get("title")
+                sqlPrice = product.get("price")
+
+                add_product = ("INSERT IGNORE INTO Data (title, price)"
+                               "VALUES (%s, %s)")
+
+                data_product = (f"{sqlTitle}", f"{sqlPrice}")
+                
+                cursor.execute(add_product, data_product)
+                print(f"Saving to MySQL: {product['title']}")
+cnx.commit()
+cursor.close()
+cnx.close()
